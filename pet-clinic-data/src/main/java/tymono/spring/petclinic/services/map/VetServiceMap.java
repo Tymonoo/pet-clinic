d314932ebdb977a921099851557521d7cf8b07be
Tymonoo <michal.tyminski14@gmail.com>
2022-07-03 15:17:31 +0200

    impl map based services, closes #6

---
+++ pet-clinic-data/src/main/java/tymono/spring/petclinic/services/map/VetServiceMap.java
@@ -0,0 +1,33 @@
+package tymono.spring.petclinic.services.map;
+
+import tymono.spring.petclinic.model.Vet;
+import tymono.spring.petclinic.services.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(),object);
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+}
