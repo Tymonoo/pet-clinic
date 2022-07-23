cc414def845a69afdf27e847072bc6d2851fe980
Tymonoo <michal.tyminski14@gmail.com>
2022-07-24 01:09:36 +0200

    adding web pages and services

---
+++ pet-clinic-data/src/main/java/tymono/spring/petclinic/services/SpecialityService.java
@@ -0,0 +1,6 @@
+package tymono.spring.petclinic.services;
+
+import tymono.spring.petclinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}
--- pet-clinic-web/src/main/java/tymono/spring/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/tymono/spring/petclinic/bootstrap/DataLoader.java
@@ -3,35 +3,92 @@ package tymono.spring.petclinic.bootstrap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
-import tymono.spring.petclinic.model.Owner;
-import tymono.spring.petclinic.model.Vet;
+import tymono.spring.petclinic.model.*;
 import tymono.spring.petclinic.services.OwnerService;
+import tymono.spring.petclinic.services.PetTypeService;
+import tymono.spring.petclinic.services.SpecialityService;
 import tymono.spring.petclinic.services.VetService;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
+    private final SpecialityService specialityService;
 
     @Autowired
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialityService specialityService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
+        this.specialityService = specialityService;
     }
 
     @Override
     public void run(String... args) throws Exception {
 
+        int count = petTypeService.findAll().size();
+
+        if (count == 0){
+            loadData();
+        }
+
+    }
+
+    private void loadData() {
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
+
+        Speciality radiology = new Speciality();
+        radiology.setDescription("Radiology");
+        Speciality savedRadiology = specialityService.save(radiology);
+
+        Speciality surgery = new Speciality();
+        surgery .setDescription("Surgery");
+        Speciality savedSurgery = specialityService.save(surgery);
+
+        Speciality dentistry = new Speciality();
+        dentistry.setDescription("Dentistry");
+        Speciality savedDentistry = specialityService.save(dentistry);
+
+
         Owner owner1 = new Owner();
         owner1.setFirstName("Micheal");
         owner1.setLastName("Weston");
+        owner1.setAddress("123 Brickerel");
+        owner1.setCity("Miami");
+        owner1.setTelephone("123123123");
+
+        Pet mikesPet = new Pet();
+        mikesPet.setPetType(savedDogPetType);
+        mikesPet.setPetOwner(owner1);
+        mikesPet.setBirthDate(LocalDate.now());
+        mikesPet.setName("Rico");
+        owner1.getPets().add(mikesPet);
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Fiona");
         owner2.setLastName("Glenanne");
+        owner2.setAddress("123 Brickerel");
+        owner2.setCity("Miami");
+        owner2.setTelephone("123123123");
+
+        Pet FionasCat = new Pet();
+        FionasCat.setPetType(savedCatPetType);
+        FionasCat.setPetOwner(owner2);
+        FionasCat.setBirthDate(LocalDate.now());
+        FionasCat.setName("Pulta");
+        owner2.getPets().add(FionasCat);
 
         ownerService.save(owner2);
 
@@ -40,12 +97,14 @@ public class DataLoader implements CommandLineRunner {
         Vet vet1 = new Vet();
         vet1.setFirstName("Sam");
         vet1.setLastName("Axe");
+        vet1.getSpecialities().add(savedRadiology);
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
         vet2.setFirstName("Jessie");
         vet2.setLastName("Porter");
+        vet2.getSpecialities().add(savedSurgery);
 
         vetService.save(vet2);
 
